cc16385d57dc663b38bf51e55f32888e4a8a2bbc
MarcoKeegan <55056594+MarcoKeegan@users.noreply.github.com>
2021-05-01 12:57:53 +0700

    slide view

--- app/src/main/java/umn/ac/id/wetix/Dashboard.java
+++ app/src/main/java/umn/ac/id/wetix/Dashboard.java
@@ -11,7 +11,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Dashboard extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -21,6 +26,16 @@ public class Dashboard extends AppCompatActivity implements BottomNavigationView
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        ImageSlider imageSlider = findViewById(R.id.slider);
+
+         List<SlideModel> slideModels = new ArrayList<>();
+         slideModels.add(new SlideModel("https://i.pinimg.com/236x/62/c4/bf/62c4bf742aa0ad91c8541da187f20486.jpg"));
+         slideModels.add(new SlideModel("https://cdn.tmpo.co/data/2018/09/23/id_735800/735800_720.jpg"));
+         slideModels.add(new SlideModel("https://cdn1-production-images-kly.akamaized.net/hcPpUU1eH_KBhJirNmYGzHwGXMw=/640x853/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3301804/original/011403400_1605845389-Asih_2.jpg"));
+         slideModels.add(new SlideModel("https://cinemags.co.id/wp-content/uploads/2019/08/Spidey-poster.jpg"));
+         imageSlider.setImageList(slideModels, true);
+
 
         toProfile = findViewById(R.id.toProfile);
         loadFragment(new HomeFragment());
