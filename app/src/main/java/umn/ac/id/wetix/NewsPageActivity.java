d018b8b9863d5c4becc2834677275396e34a7616
MarcoKeegan <55056594+MarcoKeegan@users.noreply.github.com>
2021-05-08 13:48:46 +0700

    News Page XML Commit

---
+++ app/src/main/java/umn/ac/id/wetix/NewsPageActivity.java
@@ -0,0 +1,14 @@
+package umn.ac.id.wetix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewsPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_page);
+    }
+}
